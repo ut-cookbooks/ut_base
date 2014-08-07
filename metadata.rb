9a172a0e59ecd7a2168fb4aaed60b5507ea584ae
Fletcher Nichol <fnichol@nichol.ca>
2014-08-07 16:15:05 -0600

    Update build-essential, chef-sugar, & git dep versions.

--- metadata.rb
+++ metadata.rb
@@ -11,11 +11,11 @@ supports "debian"
 supports "centos"
 supports "mac_os_x"
 
-# please see Berksfile for any special/specific versions for forks
+# please see Cheffile for any special/specific versions for forks
 depends "apt",              "~> 2.0"
-depends "build-essential",  "~> 1.4"
-depends "chef-sugar",       "~> 1.1"
-depends "git",              "~> 2.5"
+depends "build-essential",  "~> 2.0"
+depends "chef-sugar",       "~> 2.0"
+depends "git",              "~> 4.0"
 depends "homebrew",         "~> 1.4"
 depends "mosh",             "~> 0.3"
 depends "openssh",          "~> 1.2"
