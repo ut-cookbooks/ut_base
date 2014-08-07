7bf282e2487d2b8121f6cdc36269ed610a19943b
Fletcher Nichol <fnichol@nichol.ca>
2014-08-07 16:18:37 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Unicorn Tears Base"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.3.0"
+version          "1.3.1"
 
 supports "ubuntu"
 supports "debian"
